1abd3428d9c0549bf8065d239b0d5762aae681fb
komal kumari <sandilyaaparagita@gmail.com>
2025-09-01 22:48:05 +0530

    chat routes made

--- api/controllers/message.controller.js
+++ api/controllers/message.controller.js
@@ -1,19 +1,43 @@
 import prisma from "../lib/prisma.js";
-export const getChats = async (req,res)=>{
-    const tokenUserId = req.userId;
 
-    try{
-        const data = await prisma.chat.findMany({
-            where:{
-                userIDs:{
-                
-                    hasSome:[tokenUserId]
-                },
-            },
-        });
-        res.status(200).json(chats);
-    }catch(err){
-        console.log(err);
-        res.status(500).json({message:"Failed to get the chats!"})
-    }
-}
+export const addMessage = async (req, res) => {
+  const tokenUserId = req.userId;
+  const chatId = req.params.chatId;
+  const text = req.body.text;
+
+  try {
+    const chat = await prisma.chat.findUnique({
+      where: {
+        id: chatId,
+        userIDs: {
+          hasSome: [tokenUserId],
+        },
+      },
+    });
+
+    if (!chat) return res.status(404).json({ message: "Chat not found!" });
+
+    const message = await prisma.message.create({
+      data: {
+        text,
+        chatId,
+        userId: tokenUserId,
+      },
+    });
+
+    await prisma.chat.update({
+      where: {
+        id: chatId,
+      },
+      data: {
+        seenBy: [tokenUserId],
+        lastMessage: text,
+      },
+    });
+
+    res.status(200).json(message);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to add message!" });
+  }
+};
--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -76,7 +76,15 @@ function ProfilePage() {
       </div>
       <div className="chatContainer">
         <div className="wrapper">
-          <Chat/>
+          <Suspense fallback={<p>Loading...</p>}>
+          <Await
+          resolve={data.chatResponse}
+          errorElement={<p>Error loading chat</p>}
+          >
+            {(chatResponse)=><Chat chats ={chatResponse.data}/>}
+          </Await>
+          </Suspense>
+          
         </div>
       </div>
     </div>
